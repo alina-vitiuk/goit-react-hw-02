466bd6bb3c5570cfb84a539b926d48b5a08c7dab
Alina_Vitiuk <vityukalina@gmail.com>
2024-12-17 16:27:45 +0200

    fixed homework

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -26,9 +26,7 @@ function App() {
   }, [values]);
 
   const totalFeedback = values.good + values.neutral + values.bad;
-  const positiveFeedback = Math.round(
-    ((values.good + values.neutral) / totalFeedback) * 100
-  );
+  const positiveFeedback = Math.round((values.good / totalFeedback) * 100);
 
   const updateFeedback = (feedbackType) => {
     setValues({
